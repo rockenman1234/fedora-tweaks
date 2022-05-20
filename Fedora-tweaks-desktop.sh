23c099fd6123f74b9d30d99e2e6b0b9df67e8e4e
Alex Jenkins <22626564+rockenman1234@users.noreply.github.com>
2022-05-19 23:07:17 -0400

    Tweaks Desktop
    Removes track pad integration for gnome

---
+++ Fedora-tweaks-desktop.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+KERNEL_VERSION=$(uname -r)
+
+# WARNING: MUST ONLY BE USED ON FRESH FEDORA INSTALL WITH INTERNET ACCESS, AS ROOT!!! Run at your own risk! This script enables special tweaks to make fedora more usable. These tweaks include: DNF speed up, delta mirrors, installs RPM Fusion  (free and non-free), installs gnome-tweaks, fedy, TLP, steam, vlc, support for various multimedia codecs and compression support, snap, better_fonts, wine-devel, fish shell, audacity, and chromium, better touchpad and ssd support. It is broken up in the way it is for easy tweaking and fixing for future fedora versions. Made by Alex jenkins, follow me on github at https://github.com/rockenman1234 
+
+echo '
+
+
+
+'
+echo '/ _|  ___  __| | ___  _ __ __ _  | |___      _____  __ _| | _____'
+echo '| |_ / _ \/ _\ |/ _ \| |__/ _\ | | __\ \ /\ / / _ \/ _\ | |/ / __|'
+echo '|  _|  __/ (_| | (_) | | | (_| | | |_ \ V  V /  __/ (_| |   <\__ \'
+echo '|_|  \___|\__,_|\___/|_|  \__,_|  \__| \_/\_/ \___|\__,_|_|\_\___/'
+echo 'WARNING: READ THE FOLLOWING VERY CAREFULLY! MUST ONLY BE USED ON FRESH FEDORA INSTALL WITH INTERNET ACCESS, AS ROOT!!! Run at your own risk! The process could take aslong as half an hour depending on your internet connection, but should only take 3-5 minutes with good internet connection. I cannot be held responsible for a bricked install, you have been warned!'
+
+echo '
+
+
+
+'
+
+echo 'This is not a fully autonomous script, when installing RPM-Fusion repos, this script cannot accept the keys for you, you MUST type "y" When asked or the script will fail!!! Please close and save all work before running this script!'
+
+echo '
+
+
+
+'
+
+echo "Accepting the terms above, do you still wish to run? Type 1 or 2."
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+
+echo 'This script enables special tweaks to make fedora more usable. These tweaks include: DNF speed up, installs RPM Fusion  (free and non-free), installs gnome-tweaks, fedy, TLP, steam, vlc, support for various multimedia codecs and compression support, snap, better_fonts'
+
+# These following lines speed up dnf
+echo 'fastestmirror=true' >> /etc/dnf/dnf.conf
+
+echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf
+
+echo 'deltarpm=true' >> /etc/dnf/dnf.conf
+
+sudo dnf update -y
+
+# These next lines add RPM Fusion Free and Non-free repos
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+  
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+sudo dnf update -y
+
+sudo dnf groupupdate core
+
+# This line installs gnome-tweaks
+sudo dnf install gnome-tweak-tool -y
+
+# These line installs fedy
+sudo dnf copr enable kwizart/fedy -y
+
+sudo dnf install fedy -y
+
+# This line updates the rescue image
+/etc/kernel/postinst.d/51-dracut-rescue-postinst.sh ${KERNEL_VERSION} /boot/vmlinuz-${KERNEL_VERSION}
+
+# This line installs steam
+sudo dnf install steam -y
+
+# This line installs vlc
+sudo dnf install vlc -y
+
+# These lines installs various multimedia codecs, some of which might already be installed
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+
+sudo dnf groupupdate sound-and-video -y
+
+sudo dnf install rpmfusion-free-release-tainted -y
+
+sudo dnf install libdvdcss -y
+
+sudo dnf install rpmfusion-nonfree-release-tainted -y
+
+sudo dnf install \*-firmware -y
+
+# This line installs support for compression algorithims 
+sudo dnf install p7zip p7zip-plugins -y
+
+# This line installs MSCore fonts and cabextract
+sudo dnf install cabextract -y
+
+rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
+# This line installs snap
+sudo dnf install snapd -y
+
+# These lines installs and enables better_fonts
+sudo dnf copr enable dawid/better_fonts -y
+
+sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -y
+
+# This line installs OCS-URL
+rpm -i https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1MzA3NzQ2ODAiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjZmNmQ4Y2E0MzNmZTY5OTY3ZmQ4NzZhMDUzZDlmNmM1NjAzOGNkODFkMjk2NzYzODNjMDdhNmVkNTJiN2U4NDAyMTgxNjRiYzJhNTkyY2E4M2Q0M2EyYzQyMGQwZmYyY2FhYWVkY2UxNjljODI0NzhjZWY1ZmViODg2NzVkYzFjIiwidCI6MTYwMTI2MDkxOCwic3RmcCI6IjEyMDM0NDQzYzE3YzI3NzljNzUxZWM2YzQ2NTA0NTQ4Iiwic3RpcCI6IjI2MDA6NmM1YTo2MzgwOjE5MzpmOTUwOjkyMzY6ZTA6NzM3NSJ9.K_cK1APNRJ39lpuoT3Cd1SRjeoNsOPjNyC75Uphy7Ak/ocs-url-3.1.0-1.x86_64.rpm
+
+# These lines install wine
+sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/32/winehq.repo -y
+
+sudo dnf install winehq-devel -y
+
+# This line install fish shell
+sudo dnf install fish -y
+
+# This line installs audacity
+sudo dnf install audacity -y
+
+# This line installs chromium
+sudo dnf install chromium -y
+
+# These lines install dnf upgrade
+sudo dnf update -y
+
+sudo dnf upgrade --refresh -y
+
+sudo dnf check-update -y
+
+sudo dnf install dnf-plugin-system-upgrade -y
+
+# Enable SSD trimmer
+sudo systemctl enable --now fstrim.timer
+
+# This line removes unnecessary packages
+sudo dnf remove -y abrt*
+
+# Ending messages and heads-up
+
+echo '
+
+
+
+'
+
+echo 'The tweaks are done! Thanks for using my project! NOTICE: Please read if you have and NVIDIA card: https://rpmfusion.org/Howto/NVIDIA, this script cannot install nvidia drivers for you! Please either use this article (recommended), or use fedy to install the drivers you need!'
+
+echo '
+
+
+
+'
+
+echo 'Follow me on github! https://github.com/rockenman1234'
+
+echo '
+
+
+
+'
+	
+echo 'It is imperative to reboot as soon as possible!!!'
+
+# End of file
